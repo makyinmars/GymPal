4854a1953e00b67fc2c446153e5364622b996e89
Franklin Jara <franklinjaradev@gmail.com>
2022-09-24 18:00:58 -0400

    :sparkles: feat: menu for all the pages

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -2,16 +2,14 @@ import {GetServerSidePropsContext} from 'next'
 import {useSession} from 'next-auth/react'
 import {useRouter} from 'next/router'
 import {useEffect} from 'react'
+import Menu from 'src/components/menu'
 
 import {getServerAuthSession} from 'src/server/common/get-server-auth-session'
-
-import {trpc} from 'src/utils/trpc'
 
 const UserId = () => {
 	const {data: session} = useSession()
 	const router = useRouter()
 
-	console.log(session)
 	useEffect(() => {
 		if (!session) {
 			router.push('/')
@@ -19,10 +17,12 @@ const UserId = () => {
 	}, [router, session])
 
 	return (
-		<div className='container mx-auto p-4'>
-			<h1>User</h1>
-			<div className=''></div>
-		</div>
+		<Menu>
+			<div className='container mx-auto p-4'>
+				<h1>User</h1>
+				<div className=''></div>
+			</div>
+		</Menu>
 	)
 }
 
