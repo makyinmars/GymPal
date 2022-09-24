1ed5f345a2a901df938f6f1eaec1d6240c35fc19
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-09-24 12:10:45 -0400

    :sparkles: feat: menu page

---
+++ src/pages/login.tsx
@@ -0,0 +1,18 @@
+// import { FaDiscord } from 'react-icons/fa'
+
+const login = () => {
+	return (
+		<div className='bg-light container mx-auto p-4'>
+			<h1 className='text-primary'>GymPal</h1>
+			<div>
+				<div className='bg-black'></div>
+				<div>
+					<span>Login</span>
+					{/* <FaDiscord /> */}
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default login
