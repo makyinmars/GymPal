4621e38aa3944ddeacd78d62663f2e7892ea9007
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-09-23 21:34:57 -0400

    :wrench: style: changing background color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 
-			<main className='container mx-auto flex min-h-screen flex-col items-center justify-center p-4'>
+			<main className='bg-color-500 container mx-auto flex min-h-screen flex-col items-center justify-center p-4'>
 				<h1 className='text-5xl font-extrabold leading-normal text-gray-700 md:text-[5rem]'>
 					Create <span className='text-purple-300'>T4</span> App
 				</h1>
