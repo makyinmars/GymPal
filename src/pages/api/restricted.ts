3ddd8874fcb25d93d48825bb912ca7e1686877ac
Franklin Jara <franklinjaradev@gmail.com>
2022-09-30 23:02:17 -0400

    :lock: fix: security for twilio api

--- src/pages/api/restricted.ts
+++ src/pages/api/restricted.ts
@@ -1,5 +1,4 @@
 // Example of a restricted endpoint that only authenticated users can access from https://next-auth.js.org/getting-started/example
-
 import {NextApiRequest, NextApiResponse} from 'next'
 import {getServerAuthSession} from '../../server/common/get-server-auth-session'
 
--- src/pages/api/twilio.ts
+++ src/pages/api/twilio.ts
@@ -1,21 +1,30 @@
 import type {NextApiRequest, NextApiResponse} from 'next'
+import {getServerAuthSession} from 'src/server/common/get-server-auth-session'
 import {Twilio} from 'twilio'
 
 import {env} from 'src/env/server.mjs'
 
 const twilio = async (req: NextApiRequest, res: NextApiResponse) => {
-	if (req.method === 'POST') {
-		const {message, to} = req.body
-		const twilioClient = new Twilio(
-			env.TWILIO_ACCOUNT_SID,
-			env.TWILIO_AUTH_TOKEN
-		)
-		const twilioMessage = await twilioClient.messages.create({
-			body: message,
-			from: env.TWILIO_PHONE_NUMBER,
-			to: to,
+	const session = await getServerAuthSession({req, res})
+	if (session) {
+		if (req.method === 'POST') {
+			const {message, to} = req.body
+			const twilioClient = new Twilio(
+				env.TWILIO_ACCOUNT_SID,
+				env.TWILIO_AUTH_TOKEN
+			)
+			const twilioMessage = await twilioClient.messages.create({
+				body: message,
+				from: env.TWILIO_PHONE_NUMBER,
+				to: to,
+			})
+			res.status(200).json(twilioMessage)
+		}
+	} else {
+		res.send({
+			error:
+				'You must be signed in to view the protected content on this page.',
 		})
-		res.status(200).json(twilioMessage)
 	}
 }
 
